5aa594d6e83a822fbc83a54b299628a0af7f0c58
Cameron <cameron_2000@live.co.uk>
2025-05-12 18:24:16 +0100

    add first basic views

--- counter/views.py
+++ counter/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello world, this is the start of my app!")
+def homePage(request):
+    return HttpResponse("Hello world, this is the home page!")
+
+def addTrolley(request):
+    return HttpResponse("Hello World, this is the addTrolley page")
+
+def trolleyDetail(request):
+    return HttpResponse("Hello World, this is the trolley deetail page!")
+
+def statistics(request):
+    return HttpResponse("hello World, this is the statistics page!!")
