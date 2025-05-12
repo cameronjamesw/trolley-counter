a9e5b696d2df64c67aa521faa027249e8df52c60
Cameron <cameron_2000@live.co.uk>
2025-05-12 18:30:12 +0100

    update urls.py to include new views

--- counter/urls.py
+++ counter/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.homePage, name="home"),
+    path("add/", views.addTrolley, name="add trolley"),
+    path("<int:pk>/detail/", views.trolleyDetail, name="detail"),
+    path("stats/", views.statistics, name="stats"),
 ]
